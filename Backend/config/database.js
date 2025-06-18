1bcb1a0127a9ff50266df9f5f5ee98dd6ac0822e
mujanati13 <simojanati.1j@gmail.com>
2025-06-18 09:36:59 +0100

    com

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,9 +2,9 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME||"xcite",
+  process.env.DB_USER||"root",
+  process.env.DB_PASSWORD||"@#@#@#@#Test1234",
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
